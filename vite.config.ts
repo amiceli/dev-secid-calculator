0bb9a07d8bcaf2f66a40508012b42538a4c35ace
amiceli <amiceli@letsignit.com>
2025-01-17 23:20:22 +0100

    fix: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/secid-calculator',
+    base: '/dev-secid-calculator',
     css: {
         postcss: {
             plugins: [autoprefixer()],
